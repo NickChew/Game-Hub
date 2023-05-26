38fb4bf2f45b8a03420e101ad1c8503b7812eb73
NickChew <118730370+NickChew@users.noreply.github.com>
2023-05-26 14:18:59 +0100

    Changed logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,7 +10,7 @@ interface Props {
 const NavBar = ({ onSearch }:Props) => {
 	return (
 		<HStack padding="15px">
-			<Image src={logo} boxSize={"100px"} />
+			<Image src={logo} boxSize={"80px"} />
 			<SearchInput onSearch={onSearch}/>
       <ColorModeSwitch/>
 		</HStack>
