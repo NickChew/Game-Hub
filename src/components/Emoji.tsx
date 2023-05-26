7a819d195014d64f017412a6dc8cae3d2bd6f7ca
NickChew <118730370+NickChew@users.noreply.github.com>
2023-05-26 12:44:02 +0100

    added emojis

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import bullsEye from "../assets/Bulls-eye.svg";
+import thumbsUp from "../assets/thumbs-up.svg";
+import meh from "../assets/meh.svg";
+import { ImageProps, Image } from '@chakra-ui/react';
+
+
+interface Props {
+  rating: number;
+}
+
+const Emoji = ({ rating }: Props) => {
+  if (rating <3) return null;
+  const emojiMap: {[key: number]: ImageProps} = {
+    3: {src : meh, alt:"Meh!", boxSize: "35px"},
+    4: {src : thumbsUp, alt:"Recommended", boxSize: "25px"},
+    5: {src : bullsEye, alt:"Exceptional!", boxSize: "25px"}
+  }
+
+  return (
+    <Image {...emojiMap[rating]} marginTop={1} />
+  )
+}
+
+export default Emoji
